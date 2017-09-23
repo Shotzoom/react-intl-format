e4ffe266c57af12a96768e6229d9d440ff4f188b
Brenden Snyder <rstone770@gmail.com>
2017-09-23 01:05:55 -0700

    Add testing.

---
+++ source/__tests__/react-intel-format.js
@@ -0,0 +1,12 @@
+import configure from '../configure';
+import * as format from '../react-intl-format';
+
+describe('react-intl-format', () => {
+  it('exports configure as configure', () => {
+    expect(format.configure).toEqual(configure);
+  });
+
+  it('exports <Format /> as Format', () => {
+    expect(format.Format).toBeInstanceOf(Function);
+  });
+});
